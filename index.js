d08208545372aaf64af53fa00b4976abf289b6c7
Anuragsurya318 <91601354+Anuragsurya318@users.noreply.github.com>
2024-07-29 01:13:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "https://chess-royale-frontend.vercel.app", // Replace with your frontend URL
+    origin: "*", // Replace with your frontend URL
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -16,7 +16,7 @@ const io = socketIo(server, {
 // CORS middleware for Express
 app.use(
   cors({
-    origin: "https://chess-royale-frontend.vercel.app", // Replace with your frontend URL
+    origin: "*", // Replace with your frontend URL
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
